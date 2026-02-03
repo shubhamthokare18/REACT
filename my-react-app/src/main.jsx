93d37fd7007e72af81db3c050b241b6ea61aa6f1
shubhamthokare18 <shubhamthokare.eidiko@gmail.com>
2026-02-03 19:00:51 +0530

    26/06/2026

---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,35 @@
+// import { createRoot } from 'react-dom/client'
+
+// createRoot(document.getElementById('root')).render(
+//   <p>Welcome!</p>
+// ) 
+
+
+
+// import { createRoot } from 'react-dom/client'
+
+// const myelement = (
+//   <table>
+//     <tr>
+//       <th>Name</th>
+//     </tr>
+//     <tr>
+//       <td>John</td>
+//     </tr>
+//     <tr>
+//       <td>Elsa</td>
+//     </tr>
+//   </table>
+// );
+
+// createRoot(document.getElementById('root')).render(
+//   myelement
+// )
+
+
+
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('sandy')).render(
+  <p>Welcome!</p>
+) 
