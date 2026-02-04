9e68714f64c6fbdb92f2069a19cf47ea10982c7c
shubhamthokare18 <shubhamthokare.eidiko@gmail.com>
2026-02-04 19:03:30 +0530

    19-02-2026

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -28,8 +28,68 @@
 
 
 
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
 
-createRoot(document.getElementById('sandy')).render(
-  <p>Welcome!</p>
-) 
+// createRoot(document.getElementById('sandy')).render(
+//   <p>Welcome!</p>
+// ) 
+
+
+
+// import{createRoot} from 'react-dom/client'
+// const fruitlist=['apple','banana','cherry'];
+// function MyList(){
+//   return(
+//   <ul>
+//     {fruitlist.map(fruit=>
+//       <li key={fruit}>{fruit}</li>
+//     )}
+//   </ul>
+//   );
+// }
+// createRoot(document.getElementById('root')).render(
+//   <MyList/>
+// )
+
+
+
+// import{createRoot} from 'react-dom/client'
+// const users=[
+//   {id:1,name:'John',age:30},
+//   {id:2,name:'Jane',age:25}
+// ];
+// function UserList(){
+//   return(
+//     <ul>
+//       {users.map(user=>
+//         <li key={user.id}>
+//           {user.name} is {user.age} years old
+//         </li>
+//       )}
+//     </ul>
+//   );
+// }
+// createRoot(document.getElementById('root')).render(
+//   <UserList/>
+// )
+
+
+
+import{createRoot} from 'react-dom/client'
+const fruitlist=['apple','banana','cherry'];
+function App(){
+  return(
+    <ul>
+      {fruitlist.map((fruit,index,array)=>{
+        return(
+          <li key={fruit}>
+            Number: {fruit},Index: {index},Array: {array}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+createRoot(document.getElementById('root')).render(
+  <App/>
+)
